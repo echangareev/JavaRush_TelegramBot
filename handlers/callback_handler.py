5742ce0d1dd5ac8fdf6404a9d4d4c49db98a626d
Евгений Шангареев <e.changareev@gmail.com>
2026-04-05 10:34:16 +0200

    Callback functions

--- handlers/callback_handler.py
+++ handlers/callback_handler.py
@@ -6,5 +6,5 @@ router = Router()
 
 @router.callback_query(F.data == 'btn:hello')
 async def cmd_hello(callback: CallbackQuery):
-    # await callback.message.answer('you pushed the button') # answer as message
+    await callback.message.answer('you pushed the button') # answer as message
     await callback.answer('you pushed the button')  # answer as pop-up message from above
